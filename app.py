5c62795142470f55e1af7e0e6ad140ab9abdb0db
quelife <thanaphon.que@gmail.com>
2020-10-20 09:50:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,9 +77,8 @@ app = Flask(__name__)
 def hello():
     return "Hello Guys!"
 
-
-@app.route("/webhook", methods=['GET','POST'])
-def webhook():
+@app.route("/callback", methods=['POST'])
+def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -91,10 +90,27 @@ def webhook():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
+
+# @app.route("/webhook", methods=['GET','POST'])
+# def webhook():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
+
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         print("Invalid signature. Please check your channel access token/channel secret.")
+#         abort(400)
+
+#     return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
